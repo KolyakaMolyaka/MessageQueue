9d003cef4824fc9f0e8220ccfd0a68d4e808bfae
Nikolay Markeev <zz5dy6@gmail.com>
2023-04-02 13:14:28 +0300

    Bug with time_ns_diff fixed.

--- receive_messages_tools.c
+++ receive_messages_tools.c
@@ -2,7 +2,7 @@
 
 void time_ns_diff(struct timespec starttime, struct timespec endtime, long long *delta_ns) {
     // function calculates difference between two times in nanosecs
-    *delta_ns = (endtime.tv_sec - starttime.tv_sec) * 1000000000; 
+    *delta_ns += (endtime.tv_sec - starttime.tv_sec) * 1000000000; 
     *delta_ns += (endtime.tv_nsec - starttime.tv_nsec);
 }
 
